c77e999df86fbaa835cef86c6730e4ebbfd85d18
Ruhaan Sharma <142501438+GC-995@users.noreply.github.com>
2025-01-27 14:52:28 +0530

    improved cross device functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,12 +28,14 @@ function Header() {
     }
   }
   return (
-    <header className="">
+    <header className="ok123">
      <nav class="fixed top-0 left-0 w-full h-auto bg-gray-800 z-10 flex items-center justify-around">
       
-     <h3 class="relative heading font-bold md:basis-1/6 text-3xl xs:basis-4/12 z-50 mb-5 mt-5 whitespace-nowrap">
-  Brevity - News Aggregator
+     <h3 class="relative heading font-bold md:basis-1/6 text-2xl xs:basis-4/12 z-50 mb-5 mt-5 whitespace-nowrap">
+  <span class="hidden sm:block">Brevity - News Aggregator</span>
+  <span class="block sm:hidden">Brevity</span>
 </h3>
+
 
         <ul className={active ? "nav-ul flex gap-11 md:gap-14 xs:gap-12 lg:basis-3/6 md:basis-4/6 md:justify-end active" : " nav-ul flex gap-14 lg:basis-3/6 md:basis-4/6 justify-end"}>
           <li><Link className="no-underline font-semibold" to="/" onClick={() => { setActive(!active) }}>Home</Link></li>
